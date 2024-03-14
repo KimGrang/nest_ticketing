90d9a8d5ecdfc9ffa4511049b66bd14fc000b044
kimgrang <kimgrang1202@gmail.com>
2024-03-14 23:52:57 +0900

    clone

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Book } from 'src/book/entities/book.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Book]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
 })
